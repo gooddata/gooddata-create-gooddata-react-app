361b6b79a76b2cf6de508d279980d20d2c4e2488
Martin Plavek <martin.plavek@gooddata.com>
2021-08-09 09:19:22 +0200

    RAIL-3588 platform picker
    - add platform (Tiger/Bear) inquiry
    - make typescript a preferred option
    
    JIRA: RAIL-3588

--- index.js
+++ index.js
@@ -16,10 +16,7 @@ program
     .option("--no-install", "skip yarn installing the app dependencies")
     .option("--verbose", "output additional logs, useful mainly for debugging and bug reports")
     .option("--backend <backend>", "setting backend of the app (default: bear backend)")
-    .option(
-        "--flavor <flavor>",
-        "language flavor of the app, either TypeScript (ts) or JavaScript (js)",
-    )
+    .option("--flavor <flavor>", "language flavor of the app, either TypeScript (ts) or JavaScript (js)")
     .action(async appName => {
         const bootstrapFunc = program.config ? getBootstrapDataFromConfig : getBootstrapDataFromCli;
         const bootstrapData = await bootstrapFunc(appName, program);
@@ -28,7 +25,6 @@ program
             install: program.install,
             targetDir: program.targetDir,
             verbose: program.verbose,
-            backend: program.backend,
         });
     })
     .parse(process.argv);
--- src/cliBootstrap.js
+++ src/cliBootstrap.js
@@ -1,5 +1,5 @@
 // (C) 2019 GoodData Corporation
-import { inquireName, inquireHostname, inquireFlavor } from "./inquiries";
+import { inquireName, inquireHostname, inquireFlavor, inquireBackend } from "./inquiries";
 import { sanitizeAppName } from "./stringUtils";
 
 const getSanitizedAppName = async nameFromCli => {
@@ -15,17 +15,27 @@ const getSanitizedAppName = async nameFromCli => {
 const getSanitizedFlavor = async flavor => {
     const sanitizedFlavor = flavor !== "ts" && flavor !== "js" ? undefined : flavor;
     return sanitizedFlavor || inquireFlavor();
-}
+};
+
+const getSanitizedBackend = async backend => {
+    const sanitizedBackend = backend !== "bear" && backend !== "tiger" ? undefined : backend;
+    return sanitizedBackend || inquireBackend();
+};
 
 /**
  * Obtains all the information needed to bootstrap the project either from command line options or via interactive prompts.
  * @param {string} nameFromCli App name provided as the CLI argument.
  * @param {Object} options Other options from the CLI.
  */
-const getBootstrapData = async (nameFromCli, { hostname: hostnameFromCli, backend: backendFromCli, flavor: flavorFromCli }) => {
+const getBootstrapData = async (
+    nameFromCli,
+    { hostname: hostnameFromCli, backend: backendFromCli, flavor: flavorFromCli },
+) => {
     const sanitizedAppName = await getSanitizedAppName(nameFromCli);
 
-    const hostname = hostnameFromCli || (await inquireHostname(!backendFromCli || backendFromCli === "bear"));
+    const backend = await getSanitizedBackend(backendFromCli);
+
+    const hostname = hostnameFromCli || (await inquireHostname(backend === "bear"));
 
     const flavor = await getSanitizedFlavor(flavorFromCli);
 
@@ -33,6 +43,7 @@ const getBootstrapData = async (nameFromCli, { hostname: hostnameFromCli, backen
         sanitizedAppName,
         hostname,
         flavor,
+        backend,
     };
 };
 
